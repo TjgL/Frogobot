8712ec0cbaa6de4c3f85c06d9b575b6baff13f7d
TjgL <lithmoneo@gmail.com>
2026-02-10 22:27:28 +0100

    Added data project

---
+++ Frogobot.Data/FrogoContext.cs
@@ -0,0 +1,13 @@
+﻿using Frogobot.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Frogobot.Data;
+
+public class FrogoContext : DbContext
+{
+	public DbSet<User> Users { get; set; }
+
+	public FrogoContext(DbContextOptions<FrogoContext> options) : base(options)
+	{
+	}
+}
---
+++ Frogobot.Data/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Frogobot.Data.Models;
+
+public class User
+{
+	[Key]
+	public ulong DiscordUserId { get; init; }
+	
+	public int PossessPoints { get; set; } = 0;
+}
