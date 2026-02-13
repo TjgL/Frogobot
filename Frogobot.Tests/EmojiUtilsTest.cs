77e06a96ec30c99586d40465b736ba59ccc66c30
TjgL <lithmoneo@gmail.com>
2026-02-13 20:22:02 +0100

    Added Unit Tests for EmojiUtils

---
+++ Frogobot.Tests/EmojiUtilsTest.cs
@@ -0,0 +1,24 @@
+﻿using Frogobot.Core.Utils;
+
+namespace Frogobot.Tests;
+
+public class EmojiUtilsTest
+{
+	[Test]
+	public void EnsureEmojiDataIsValid()
+	{
+		var (name, id) = EmojiUtils.GetEmojiDataFromName("<:emoji_name:1234567890>");
+        
+		using (Assert.EnterMultipleScope())
+        {
+            Assert.That(name, Is.EqualTo("emoji_name"));
+            Assert.That(id, Is.EqualTo(1234567890));
+        }
+    }
+	
+	[Test]
+	public void EnsureEmojiDataIsValid_ThrowsExceptionForInvalidEmojiName()
+	{
+		Assert.Throws<ArgumentException>(() => EmojiUtils.GetEmojiDataFromName("invalid_emoji_name"));
+	}
+}
