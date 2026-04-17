0e54553fdb3c92ae8f493be3c6672c63311f990b
TjgL <42710542+TjgL@users.noreply.github.com>
2026-04-17 18:14:27 +0000

    The gambling update (#20)

---
+++ Frogobot.Core/Commands/RouletteCommands.cs
@@ -0,0 +1,53 @@
+﻿using Frogobot.Core.Utils;
+using Frogobot.Data.Services;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Options;
+using NetCord.Services.ApplicationCommands;
+
+namespace Frogobot.Core.Commands;
+
+public class RouletteCommands : ApplicationCommandModule<ApplicationCommandContext>
+{
+    private readonly IPossessPointService _possessPointService;
+    private readonly IRouletteService _rouletteService;
+    private readonly DiscordEmojiOptions _emoji;
+
+    [UsedImplicitly(ImplicitUseKindFlags.InstantiatedNoFixedConstructorSignature)]
+    public RouletteCommands(IPossessPointService possessPointService, IRouletteService rouletteService, IOptions<DiscordEmojiOptions> emojiOptions)
+    {
+        _possessPointService = possessPointService;
+        _rouletteService = rouletteService;
+        _emoji = emojiOptions.Value;
+    }
+
+    [SlashCommand("roulette", "Misez vos Possess-Points à la roulette !")]
+    public async Task<string> Roulette(
+        [SlashCommandParameter(Name = "mise", Description = "La mise en Possess-Points")] int bet, 
+        [SlashCommandParameter(Name = "type", Description = "Le type de pari à effectuer")] RouletteBetType type = RouletteBetType.Red,
+        [SlashCommandParameter(Name = "numéro", Description = "Le numéro à miser (pour le pari sur un nombre)")] int? number = null)
+    {
+        if (bet <= 0)
+            return "Vous devez miser au moins 1 point !";
+
+        if (type == RouletteBetType.Number && (number is < 0 or > 36))
+            return "Le numéro doit être compris entre 0 et 36 !";
+
+        if (type == RouletteBetType.Number && number == null)
+            return "Vous devez spécifier un numéro pour ce type de pari !";
+
+        var userPoints = await _possessPointService.GetUserPossessPointsAsync(Context.User.Id);
+
+        if (userPoints < bet)
+            return $"Vous n'avez pas assez de points ! (Points actuels : **{userPoints}** {_emoji.PossessPoint})";
+
+        var result = _rouletteService.Play(bet, type, number);
+        var newTotal = await _possessPointService.IncreasePossessPointsAsync(Context.User.Id, result.Delta);
+
+        string rollInfo = $"Le numéro tiré est le **{result.RolledNumber}**.";
+
+        if (result.IsWin)
+            return $"🎰 **GAGNÉ !** 🎰\n{rollInfo}\nVous avez gagné **{result.Delta}** {_emoji.PossessPoint} !\nVotre nouveau solde est de **{newTotal}** {_emoji.PossessPoint}.";
+        
+        return $"💀 **PERDU...** 💀\n{rollInfo}\nVous avez perdu **{bet}** {_emoji.PossessPoint}...\nVotre nouveau solde est de **{newTotal}** {_emoji.PossessPoint}.";
+    }
+}
---
+++ Frogobot.Core/Utils/RouletteService.cs
@@ -0,0 +1,72 @@
+﻿using NetCord.Services.ApplicationCommands;
+
+namespace Frogobot.Core.Utils;
+
+public enum RouletteBetType
+{
+    [SlashCommandChoice(Name = "Rouge")]
+    Red,
+    [SlashCommandChoice(Name = "Noir")]
+    Black,
+    [SlashCommandChoice(Name = "Pair")]
+    Even,
+    [SlashCommandChoice(Name = "Impair")]
+    Odd,
+    [SlashCommandChoice(Name = "Nombre")]
+    Number
+}
+
+public interface IRouletteService
+{
+    RouletteResult Play(int bet, RouletteBetType type, int? number = null);
+}
+
+public record RouletteResult(bool IsWin, int Delta, int? RolledNumber = null);
+
+public class RouletteService : IRouletteService
+{
+    private readonly Random _random;
+
+    private static readonly int[] RedNumbers = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
+
+    public RouletteService(Random? random = null)
+    {
+        _random = random ?? Random.Shared;
+    }
+
+    public RouletteResult Play(int bet, RouletteBetType type, int? number = null)
+    {
+        int roll = _random.Next(0, 37);
+
+        bool isWin = false;
+        int multiplier = 0;
+
+        switch (type)
+        {
+            case RouletteBetType.Red:
+                isWin = roll != 0 && RedNumbers.Contains(roll);
+                multiplier = 2;
+                break;
+            case RouletteBetType.Black:
+                isWin = roll != 0 && !RedNumbers.Contains(roll);
+                multiplier = 2;
+                break;
+            case RouletteBetType.Even:
+                isWin = roll != 0 && roll % 2 == 0;
+                multiplier = 2;
+                break;
+            case RouletteBetType.Odd:
+                isWin = roll != 0 && roll % 2 != 0;
+                multiplier = 2;
+                break;
+            case RouletteBetType.Number:
+                isWin = roll == number;
+                multiplier = 36;
+                break;
+        }
+
+        int delta = isWin ? bet * (multiplier - 1) : -bet;
+
+        return new RouletteResult(isWin, delta, roll);
+    }
+}
